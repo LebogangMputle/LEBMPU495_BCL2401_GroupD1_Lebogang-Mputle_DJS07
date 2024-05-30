c7cd6cbc9fdd643f67ccf564465b102ba888d2a6
LebogangMputle <lebozikalala@gmail.com>
2024-05-30 15:09:48 +0200

    "Refactor Meme component to use state for meme data and image URL"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,13 +1,23 @@
 import memesData from "../memesData.jsx"
 
 export default function Meme() {
-    //const [memeImage, setMemeImage] = React.useState("")
-
-    let url
-
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
 
     return (
